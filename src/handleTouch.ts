220ecbf62f9965f17ac3d7f11bdb1f85dddef13a
NickRoach <njlroach@gmail.com>
2023-09-23 14:44:09 +1000

    limit touches to raycast render, don't move when touch is almost still

--- src/handleTouch.ts
+++ src/handleTouch.ts
@@ -1,21 +1,28 @@
+import { raycastHeight, raycastLeft, raycastTop } from "./constants"
+
 export const handleTouchMove = (e, keyPresses, touches) => {
 	const { lastTouch } = touches
 	const touch = e.touches[0]
-	if (lastTouch) {
-		if (touch.clientX > lastTouch.clientX) {
+	const isInRaycastRender =
+		touch.clientX > raycastLeft &&
+		touch.clientY > raycastTop &&
+		touch.clientX < raycastLeft + raycastHeight &&
+		touch.clientY < raycastTop + raycastHeight
+	if (lastTouch && isInRaycastRender) {
+		if (touch.clientX > lastTouch.clientX + 3) {
 			keyPresses.right = true
 			keyPresses.left = false
-		} else if (touch.clientX < lastTouch.clientX) {
+		} else if (touch.clientX < lastTouch.clientX - 3) {
 			keyPresses.left = true
 			keyPresses.right = false
 		} else {
 			keyPresses.left = false
 			keyPresses.right = false
 		}
-		if (touch.clientY > lastTouch.clientY) {
+		if (touch.clientY > lastTouch.clientY + 3) {
 			keyPresses.down = true
 			keyPresses.up = false
-		} else if (touch.clientY < lastTouch.clientY) {
+		} else if (touch.clientY < lastTouch.clientY - 3) {
 			keyPresses.up = true
 			keyPresses.down = false
 		} else {
