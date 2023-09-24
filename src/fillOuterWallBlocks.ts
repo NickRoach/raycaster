51b43dc1913d4eb8f7d5065119cf18fc5d8ead46
NickRoach <njlroach@gmail.com>
2023-09-25 09:19:27 +1000

    put some gaps in the outer wall

--- src/fillOuterWallBlocks.ts
+++ src/fillOuterWallBlocks.ts
@@ -1,20 +1,30 @@
 import { Block } from "./types"
 
 export const fillOuterWallBlocks = (blockArray: [Block[]]) => {
+	const getStateProbability = () => {
+		return Math.random() > 0.2
+	}
+
+	const getColor = () => {
+		return Math.random() > 0.5 ? "#38475C" : "#354459"
+	}
+
+	const wallBlockHeight = 1.2
+
 	//sides
 	for (let x = 0; x < blockArray.length; x += blockArray.length - 1) {
 		for (let y = 0; y < blockArray[0].length; y++) {
-			blockArray[x][y].state = true
-			blockArray[x][y].color = Math.random() > 0.5 ? "#38475C" : "#354459"
-			blockArray[x][y].height = 0.1
+			blockArray[x][y].state = getStateProbability()
+			blockArray[x][y].color = getColor()
+			blockArray[x][y].height = wallBlockHeight
 		}
 	}
 	// top and bottom
 	for (let y = 0; y < blockArray[0].length; y += blockArray[0].length - 1) {
 		for (let x = 1; x < blockArray.length - 1; x++) {
-			blockArray[x][y].state = true
-			blockArray[x][y].color = Math.random() > 0.5 ? "#38475C" : "#354459"
-			blockArray[x][y].height = 0.1
+			blockArray[x][y].state = getStateProbability()
+			blockArray[x][y].color = getColor()
+			blockArray[x][y].height = wallBlockHeight
 		}
 	}
 }
